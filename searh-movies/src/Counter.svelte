53afd7105cfc897c978553aa17f59081119823e1
Jonathan Rod <jonyrm91@gmail.com>
2022-03-26 21:08:51 +0100

    renderizando lista de peliculas

--- searh-movies/src/Counter.svelte
+++ searh-movies/src/Counter.svelte
@@ -24,20 +24,6 @@
 <button on:click={handleClic}>Incrementar</button>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  span {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
   @media (min-width: 640px) {
     main {
       max-width: none;
