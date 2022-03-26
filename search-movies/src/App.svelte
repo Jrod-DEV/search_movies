dd94b813e2349db2e0a4d3c330ec7189f5701f9b
Jonathan Rod <jonyrm91@gmail.com>
2022-03-26 22:29:46 +0100

    search movies app with Svelte

---
+++ search-movies/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Input from './Input.svelte';
+</script>
+
+<section>
+  <h1>Search Movies 🍿</h1>
+  <Input />
+</section>
+
+<style>
+  h1 {
+    font-size: 2rem;
+  }
+  section {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
